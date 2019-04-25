35df40d32489e255e12e04cae4cd41ce145616df
performanceArtist1 <nakedant19@gmail.com>
2019-04-25 14:09:04 +0600

    Fix links and paths

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ const config = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/[name].js',
     publicPath: '/'
-    //publicPath: '/test/'
+    //publicPath: '/ui/'
   },
 
   devServer: {
