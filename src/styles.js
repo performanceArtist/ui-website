37cf76c208cf93846e2e00bd1a06c047eeda2e73
performanceArtist1 <nakedant19@gmail.com>
2019-04-27 13:09:46 +0600

    Fix footer, header tags

--- src/styles.js
+++ src/styles.js
@@ -2,6 +2,8 @@ import './static/fonts/myriad_pro_light/fonts.css';
 import './static/fonts/myriad_pro_bold/fonts.css';
 import 'font-awesome/scss/font-awesome.scss';
 
+import './favicons';
+
 import 'jquery-ui-dist/jquery-ui.min.css';
 import './plugins/jquery-switcher/css/switcher.css';
 import './plugins/progressbar/jquery.progressbar.css';
