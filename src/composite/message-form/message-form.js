e31c0ddf72501756c2937f697d8002e2bc4b8e35
performanceArtist1 <nakedant19@gmail.com>
2019-08-06 17:06:53 +0600

    Fix message-form component

--- src/composite/message-form/message-form.js
+++ src/composite/message-form/message-form.js
@@ -8,20 +8,26 @@ class MessageForm {
   }
 
   init() {
+    const nameBubble = document.createElement('div');
+    this.root
+      .querySelector('.message-form__name .message-form__bubble')
+      .appendChild(nameBubble);
+
     this.name = {
       regex: /^[a-zA-Z0-9]+$/,
       input: this.root.querySelector('.message-form__name input'),
-      bubble: this.root.querySelector(
-        '.message-form__name .message-form__bubble'
-      )
+      bubble: nameBubble
     };
+
+    const emailBubble = document.createElement('div');
+    this.root
+      .querySelector('.message-form__email .message-form__bubble')
+      .appendChild(emailBubble);
 
     this.email = {
       regex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       input: this.root.querySelector('.message-form__email input'),
-      bubble: this.root.querySelector(
-        '.message-form__email .message-form__bubble'
-      )
+      bubble: emailBubble
     };
 
     this.name.input.addEventListener('input', () => this.validate(this.name));
@@ -29,7 +35,7 @@ class MessageForm {
   }
 
   validate(field) {
-    const valid = field.regex.test(current.input.value);
+    const valid = field.regex.test(field.input.value);
     const newClass = valid ? 'bubble_valid' : 'bubble_invalid';
     const newText = valid ? 'thanks' : 'error';
 
